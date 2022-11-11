46f0bcfc3542bbace8508300aac0501954ce15ac
Thiago Meira <thiagoluka@hotmail.com>
2022-11-11 11:58:58 -0300

    Small fix InventoryPage and new main

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-from controllers.MainController import MainController
+from user_interfaces.MainUI import MainUI
+from user_interfaces.GenericUI import GenericUI
+from steam_users.SteamUserController import SteamUserController
+from web_crawlers.ProfileBadgesPage import ProfileBadgesPage
+from web_crawlers.InventoryPage import InventoryPage
+from db.DBController import DBController
 
 
 if __name__ == '__main__':
 
-    MainController().run_ui()
+    SteamUserController()
+    DBController()
+
+    while True:
+        command = MainUI.run()
+
+        if command == 1:
+            SteamUserController().run_ui()
+        if command == 2:
+            user = SteamUserController().get_active_user()
+            user.scrap(ProfileBadgesPage(), logged_in=True)
+        if command == 3:
+            # command should use game_name instead of booster_pack_class_id
+            # game_name = MainUI.get_game_name()
+            booster_pack_id = GenericUI.get_string('Digite o class_id do booster pack: ')
+            user = SteamUserController().get_active_user()
+            if not user.inventory_downloaded():
+                user.scrap(InventoryPage())
+            interaction = {
+                'type': 'open_booster_pack',
+                'game_name': '',
+                'booster_pack_class_id': booster_pack_id,
+            }
+            user.interact(InventoryPage(), interaction)
+
+        if command == 0:
+            MainUI.goodbye()
+            break
--- web_crawlers/InventoryPage.py
+++ web_crawlers/InventoryPage.py
@@ -1,20 +1,20 @@
 import requests
 
-from web_crawlers.SteamWebPage import SteamWebPage
 from user_interfaces.GenericUI import GenericUI
+from web_crawlers.SteamWebPage import SteamWebPage
 from data_models.SteamInventory import SteamInventory
 
 
 class InventoryPage(SteamWebPage):
 
     def requires_login(self) -> bool:
-        return False
+        return True
 
     def required_user_data(self, interaction_type: str, logged_in: bool = False) -> dict:
         interactions = {
             'scrap': {
                 'standard': ['steam_id'],
-                'cookies': [],
+                'cookies': ['steamMachineAuth', 'steamLoginSecure'],
             },
             'open_booster_pack': {
                 'standard': ['steam_alias', 'inventory'],
@@ -30,23 +30,27 @@ class InventoryPage(SteamWebPage):
         return ['open_booster_pack']
 
     def scrap(self, user_data: dict, cookies: dict):
+        # Extract
         full_inventory_raw = self.__download_full_inventory(user_data['steam_id'], cookies)
+
+        # Transform
         inventory = SteamInventory.from_inventory_page(full_inventory_raw)
+
         return inventory
 
     def interact(self, action: dict, user_data: dict):
         if action['type'] == 'open_booster_pack':
             self.__open_booster_pack(
                 action['game_name'],
-                action['booster_pack_item_id'],
+                action['booster_pack_class_id'],
                 user_data['steam_alias'],
                 user_data['inventory'],
                 user_data['cookies'],
             )
 
     def __download_full_inventory(self, steam_id: str, cookies: dict) -> dict:
-        print("Downloading today's inventory...")
-        GenericUI.progress_completed(progress=0, total=1)
+        progress_text = 'Downloading inventory'
+        GenericUI.progress_completed(progress=0, total=1, text=progress_text)
 
         progress_counter = 0
         items_per_page = 2000
@@ -55,8 +59,10 @@ class InventoryPage(SteamWebPage):
         inventory_page = requests.get(first_page_url, cookies=cookies).json()
 
         inventory_size = inventory_page['total_inventory_count']
+
         progress_counter += 1
-        GenericUI.progress_completed(progress=progress_counter * items_per_page, total=inventory_size)
+        progress = progress_counter * items_per_page
+        GenericUI.progress_completed(progress=progress, total=inventory_size, text=progress_text)
 
         full_inventory_pages_raw = inventory_page
         while 'more_items' in inventory_page.keys():
@@ -67,20 +73,20 @@ class InventoryPage(SteamWebPage):
             full_inventory_pages_raw['descriptions'].extend(inventory_page['descriptions'])
 
             progress_counter += 1
-            GenericUI.progress_completed(progress=progress_counter * items_per_page, total=inventory_size)
-        GenericUI.progress_completed(progress=1, total=1)
-        # print(inventory_pages_raw['total_inventory_count'], len(inventory_pages_raw['assets']))
+            progress = progress_counter * items_per_page
+            GenericUI.progress_completed(progress=progress, total=inventory_size, text=progress_text)
+        GenericUI.progress_completed(progress=1, total=1, text=progress_text)
 
         return full_inventory_pages_raw
 
-    def __open_booster_pack(self, game_name: str, booster_pack_item_id: str, steam_alias: str,
+    def __open_booster_pack(self, game_name: str, booster_pack_class_id: str, steam_alias: str,
                             inventory: SteamInventory, cookies: dict):
-        print('Opening booster packs...')
-        GenericUI.progress_completed(progress=0, total=1)
+        progress_text = 'Opening booster packs'
+        GenericUI.progress_completed(progress=0, total=1, text=progress_text)
 
-        # function below: returns item_id of booster pack from that game
+        # function below should return class_id of booster pack from that game. Should it be here?
         # booster_pack_id = db.get_booster_pack_id(game_name)  # booster_pack_id should come from db
-        asset_id_list = inventory.get_all_asset_id(booster_pack_item_id)
+        asset_id_list = inventory.get_all_asset_id(booster_pack_class_id)
 
         url = f"{super().BASESTEAMURL}id/{steam_alias}/ajaxunpackbooster/"
         for counter, asset_id in enumerate(asset_id_list):
@@ -90,4 +96,4 @@ class InventoryPage(SteamWebPage):
             }
             headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
             requests.post(url, data=payload, headers=headers, cookies=cookies)
-            GenericUI.progress_completed(progress=counter + 1, total=len(asset_id_list))
+            GenericUI.progress_completed(progress=counter + 1, total=len(asset_id_list), text=progress_text)
